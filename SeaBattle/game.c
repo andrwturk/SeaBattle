e8f65c174fe57d0f12b627a98fbb522df6937daa
Maksym Osadchyi <maxym.osadchy@gmail.com>
2015-03-19 17:57:29 +0200

    Sync (just for move to home laptop)

--- SeaBattle/game.c
+++ SeaBattle/game.c
@@ -15,12 +15,37 @@ int main(int argc, char const *argv[])
 void gameMain() { // game entry point
 
 	// game main cycle
+
+	
 	initField();
 	placeShips();
 
 	setPlayerName("Andrew");
 
 	Coord c;
+
+	printField();
+
+	//Первым ходит человек;
+
+	c = humanMove();
+
+	CellType cell;
+	cell = shoot(COMPUTER, c.x, c.y);	
+	
+	if (cell == CHECKED_CELL)
+	{
+		printf("You've already shot in this cell\n");
+	}
+
+
+	if (cell == SHIP_CELL) 
+	{
+		printf("Good job! You've hurted the ship!\n");
+	}
+	
+
+
 	
 	c = computerMove();
 	shoot(HUMAN, c.x, c.y);
@@ -29,12 +54,8 @@ void gameMain() { // game entry point
 	// to determine that Ship was killed, store last success computer move, than make another move.
 	// if another move is empty cell --> ship was killed, so call tellComputerShipKilled()
 
-	c = humanMove();
-	CellType cell;
-	cell = shoot(COMPUTER, c.x, c.y);	
-	if (cell == SHIP_CELL) {
-		// congratulate human
-	}
 	
-	printField();
+	
+	
+	
 }
