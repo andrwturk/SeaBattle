45eebd7c69f3892228dca7a1d585974226a12bc5
xrecord <shyshkin_konstantin@mail.ru>
2015-03-20 09:05:03 +0200

    wrote part of the draft

--- SeaBattle/shipPlacer.c
+++ SeaBattle/shipPlacer.c
@@ -5,14 +5,98 @@
 int countShipCells() {
 	return 0;
 }
+void placeShip_Cells(int);
+check_possibility(int,int);
+
+srand(time(NULL)); // Инициализируем генератор случайных чисел;
+
+cord p;
+int direction;
 
 void placeShips() {
-	placeShips_4_Cells(); //разставляет четырех палубные
-	placeShips_3_Cells(); //разставляет трьохпалубные
-	placeShips_2_Cells(); //разставляет двухпалубные
-	placeShips_1_Cells(); //разставляет однопалубные
+	if (FIELD_LENGTH >= 10)
+	{	for (int i=1; i <= 1, i++)
+		{
+			placeShip_Cells(4); //ставит четырехпалубный
+		}
+		for (int i=1; i <= 2, i++)
+		{
+			placeShip_Cells(3);
+		}
+		for (int i=1; i <= 3, i++)
+		{
+			placeShip_Cells(2);
+		}
+		for (int i=1; i <= 4, i++)
+		{
+			placeShip_Cells(1);
+		}
+	}
+	else if (FIELD_LENGTH = 8)
+	{	for (int i=1; i <= 0, i++)
+		{
+			placeShip_Cells(4); //ставит четырехпалубный
+		}
+		for (int i=1; i <= 1, i++)
+		{
+			placeShip_Cells(3);
+		}
+		for (int i=1; i <= 2, i++)
+		{
+			placeShip_Cells(2);
+		}
+		for (int i=1; i <= 3, i++)
+		{
+			placeShip_Cells(1);
+		}
+	}
+	else if (FIELD_LENGTH = 6)
+	{	for (int i=1; i <= 0, i++)
+		{
+			placeShip_Cells(4); //ставит четырехпалубный
+		}
+		for (int i=1; i <= 0, i++)
+		{
+			placeShip_Cells(3);
+		}
+		for (int i=1; i <= 1, i++)
+		{
+			placeShip_Cells(2);
+		}
+		for (int i=1; i <= 2, i++)
+		{
+			placeShip_Cells(1);
+		}
+	}
+
 }
-/*	setShipCell(HUMAN, 0,1);
+void placeShip_Cells(int quanity){
+	
+	p.x = rand () %FIELD_LENGTH;
+	p.y = rand () %FIELD_LENGTH;
+	if (computerField[p.x][p.y] == 0)
+		{
+			direction = rand () %2; // если 0 то в направлении х, если 1 то в направлении y
+			if (check_possibility(direction,quanity) == 1) //проверяем возможность размещения корабля
+			{
+				for (int i = 0; i <= (quanity-1); i++)
+				{	if (direction  == 0)
+						setShipCell(COMPUTER, p.x+i,(p.y));
+					else if (direction  == 1)
+						setShipCell(COMPUTER, p.x,(p.y+i));
+					else 
+					return 0;
+				}
+				return 1;
+			}
+			
+		}
+}
+check_possibility(int dir,int qnt)
+{
+	//нужно написать проверялщика 
+}
+/*	setShipCell(COMPUTER, p.x,py);
 	setShipCell(HUMAN, 3,3);
 	setShipCell(HUMAN, 4,3);
 	setShipCell(HUMAN, 5,3);
