c0cc555dc799bdb2f1e47108a1530b5876abac15
Maksym Osadchyi <maxym.osadchy@gmail.com>
2015-03-24 22:27:45 +0200

    t

--- SeaBattle/Makefile.bat
+++ SeaBattle/Makefile.bat
@@ -1,2 +1,2 @@
 game: stringOperations.c field.c computer.c human.c shipPlacer.c game.c
-	gcc -o game stringOperations.c field.c computer.c human.c shipPlacer.c game.c
+	gcc -g -o game stringOperations.c field.c computer.c human.c shipPlacer.c game.c
