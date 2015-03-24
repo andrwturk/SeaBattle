e91f1ad8300bd3340c720619771c8d4c0ea63684
guillermohache <guillermohache@users.noreply.github.com>
2015-03-24 12:58:41 +0200

    Update and rename me vees to testlogic.c

---
+++ testlogic.c
@@ -0,0 +1,17 @@
+#include "computer.h"
+#include "human.h"
+#include "coord.h"
+#include <stdio.h>
+int x;
+
+main()
+{
+    printf("Hello world\n");    
+    for ( x = 0; x < 100; x++ ) {
+    computerMove();
+    printf("%d\n", c.x);
+    printf("%d\n", c.y);
+    //tellComputerShipHurted();  
+    }  
+
+}
